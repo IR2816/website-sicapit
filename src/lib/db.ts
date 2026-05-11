b74c965d8550121b93104b4379f689179d7a71f6
IR2816 <irfanthebb816@gmail.com>
2026-05-11 13:11:14 +0700

    feat: forgot that this website is frontend only, disable prism accelerateurl

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,8 +6,6 @@ const globalForPrisma = globalThis as unknown as {
 
 export const db =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    accelerateUrl: process.env.DATABASE_URL ?? '',
-  })
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
